18f491bbb168eb58f012f2dcd3cec819141a1510
Rakibul Hasan Joy <rhj281@gmail.com>
2022-11-06 23:41:49 +0600

    first commit

---
+++ lib/ui/bottom_nav_pages/cart.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/widgets/fetchProducts.dart';
+
+class Cart extends StatefulWidget {
+  @override
+  _CartState createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: fetchData("users-cart-items"),
+      ),
+    );
+  }
+}
